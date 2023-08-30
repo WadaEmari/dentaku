c86cffb82e8292fb6e7013f86f5af75399c0c21a
WadaEmari <143517130+WadaEmari@users.noreply.github.com>
2023-08-30 22:25:21 +0900

    Add files via upload

---
+++ mission5-1.js
@@ -0,0 +1,14 @@
+let result = document.getElementById('result');
+  
+function appendToResult(value) {
+    result.value += value;
+ }
+
+function calculateResult(){
+  result.value = eval(result.value);
+} 
+
+function clearResult() {
+  result.value = '';
+}
+
